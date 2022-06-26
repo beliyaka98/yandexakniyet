113d7464ebfb6395bce901dbe18d23d83b184e5f
beliyaka98 <51025680+beliyaka98@users.noreply.github.com>
2022-06-27 05:19:11 +0600

    Delete yandex directory

--- yandex/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('imports', views.ApiView.as_view()),
-    path('delete/<uuid:uid>', views.ApiView.as_view()),
-    path('nodes/<uuid:uid>', views.ApiView.as_view()),
-    path('sales', views.OptionalApiView.as_view())
-]
